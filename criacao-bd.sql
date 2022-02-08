f417909f487b215cdeba7a93ab38bd4ce230e5d7
Raphael Micucci <raphaelmicucci@gmail.com>
2022-02-08 20:21:00 -0300

    tabelas criadas e clientes adicionados

---
+++ criacao-bd.sql
@@ -0,0 +1,67 @@
+CREATE TABLE cliente(
+   id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+   nome VARCHAR(100) NOT NULL, 
+   email VARCHAR(70) NOT NULL UNIQUE, 
+   senha VARCHAR(20) NOT NULL, 
+   cpf VARCHAR(15) NOT NULL UNIQUE
+);
+
+CREATE TABLE departamento(
+    codigo INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    nome   VARCHAR(50) NOT NULL, 
+    descricao TEXT
+);
+
+CREATE TABLE endereco(
+    num_seq    INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    tipo       VARCHAR(5) NOT NULL, 
+    logradouro VARCHAR(45) NOT NULL, 
+    numero     INTEGER, 
+    complemento VARCHAR(20), 
+    bairro     VARCHAR(30), 
+    cidade     VARCHAR(50),
+    estado     VARCHAR(2), 
+    cep        VARCHAR(10), 
+    cliente_id INTEGER NOT NULL,
+    CONSTRAINT endereco_cliente FOREIGN KEY 
+    (cliente_id) REFERENCES cliente(id)
+);
+
+CREATE TABLE pedido(
+    numero INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    status VARCHAR(1) NOT NULL, 
+    data_pedido DATE, 
+    valor_bruto DOUBLE, 
+    desconto    DOUBLE, 
+    valor_final   DOUBLE, 
+    cliente_id INTEGER NOT NULL,
+    CONSTRAINT cliente_pedido FOREIGN KEY 
+    (cliente_id) REFERENCES cliente(id)
+);
+
+CREATE TABLE produto(
+    codigo INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    nome VARCHAR(45) NOT NULL, 
+    descricao TEXT, 
+    preco DOUBLE, 
+    estoque INTEGER, 
+    link_foto VARCHAR(255), 
+    departamento_codigo INTEGER NOT NULL,
+    CONSTRAINT produto_depto FOREIGN KEY 
+    (departamento_codigo) REFERENCES departamento(codigo)
+);
+
+CREATE TABLE item_pedido(
+    num_sequencial INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    quantidade INTEGER, 
+    valor_unitario DOUBLE, 
+    valor_total DOUBLE, 
+    produto_codigo INTEGER NOT NULL, 
+    pedido_numero INTEGER NOT NULL,
+    CONSTRAINT item_produto FOREIGN KEY 
+    (produto_codigo) REFERENCES produto(codigo),
+    CONSTRAINT item_pedido FOREIGN KEY 
+    (pedido_numero) REFERENCES pedido(numero)
+);
+
+USE cliente;
---
+++ exercicio-1.sql
@@ -0,0 +1,50 @@
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Edan Heaton", "danny@hotmail.com", "1234", "263.608.526-24");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Lilly-Rose Hewitt", "mhanoh@yahoo.com", "1234", "333.330.252-72");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Allen Lister", "mbswan@yahoo.com", "1234", "486.563.770-27");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Daniaal Duke", "moxfulder@att.net", "1234", "206.559.375-01");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Gracey Bone", "crypt@comcast.net", "1234", "331.043.714-07");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Margaux Spears", "pedwards@aol.com", "1234", "827.122.626-62");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Rudi Knox", "pavel@live.com", "1234", "328.354.333-09");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Allan Barnett", "sbmrjbr@outlook.com", "1234", "478.814.446-81");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Pio Ermete Neri", "kewley@live.com", "1234", "911.661.977-35");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Inácio Marco Abbadelli", "pakaste@hotmail.com", "1234", "273.776.527-72");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Rossana Valente Nespoli", "flakeg@att.net", "1234", "727.860.194-00");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Amor Giulia Belo", "flavell@mac.com", "1234", "552.683.377-18");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Tito Alexandrina Ardovini", "sumdumass@icloud.com", "1234", "517.195.172-15");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Kavita Hendrix", "bastian@gmail.com", "1234", "019.337.382-35");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Sumayya Horner", "yamla@yahoo.ca", "1234", "618.144.552-80");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Rianne Sparrow", "north@outlook.com", "1234", "294.359.755-42");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Henna Freeman", "gamma@att.net", "1234", "918.118.497-25");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Hester Hurst", "giafly@hotmail.com", "1234", "324.888.711-76");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Fallon May", "richard@icloud.com", "1234", "792.512.852-68");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Maiya Patterson", "geoffr@yahoo.com", "1234", "514.567.741-31");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Kian Wheatley", "hachi@msn.com", "1234", "726.177.688-21");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Tori Reeves", "sinclair@yahoo.ca", "1234", "816.929.595-57");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Lucy Bowen", "mirod@att.net", "1234", "183.562.194-50");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Gabrielius Alexander", "stern@comcast.net", "1234", "649.142.596-72");
+INSERT INTO cliente (id, nome, email, senha, cpf)
+VALUES (NULL, "Leighton Oconnor", "kimvette@gmail.com", "1234", "718.441.324-55");
