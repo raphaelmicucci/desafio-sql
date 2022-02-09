fc666f345950be8fac06a83f6bc6effe2863b3a8
Raphael Micucci <raphaelmicucci@gmail.com>
2022-02-09 20:59:07 -0300

    todos os produtos adicionados

--- exercicio-1.sql
+++ exercicio-1.sql
@@ -1,4 +1,4 @@
-/* INSERINDO DADOS NA TABELA CLIENTE */
+/* INSERINDO DADOS NA TABELA CLIENTE (10 INSERÇÕES) */
 
 INSERT INTO cliente
 VALUES (NULL, "Edan Heaton", "danny@hotmail.com", "1234", "263.608.526-24");
@@ -21,7 +21,7 @@ VALUES (NULL, "Pio Ermete Neri", "kewley@live.com", "1234", "911.661.977-35");
 INSERT INTO cliente
 VALUES (NULL, "Inácio Marco Abbadelli", "pakaste@hotmail.com", "1234", "273.776.527-72");
 
-/* INSERINDO DADOS NA TABELA DEPARTAMENTO */
+/* INSERINDO DADOS NA TABELA DEPARTAMENTO (5 INSERÇÕES) */
 
 INSERT INTO departamento 
 VALUES (NULL, "Ficção","Ficção é o termo usado para designar uma narrativa imaginária, irreal, ou para redefinir obras criadas a partir da imaginação. Obras ficcionais podem ser parcialmente baseadas em fatos, mas sempre contêm algum conteúdo imaginário.");
@@ -34,7 +34,7 @@ VALUES (NULL, "História","História é a ciência que estuda o ser humano e sua
 INSERT INTO departamento 
 VALUES (NULL, "Ciências","Ciência refere-se a qualquer conhecimento ou prática sistemáticos. Em sentido estrito, ciência refere-se ao sistema de adquirir conhecimento baseado no método científico bem como ao corpo organizado de conhecimento conseguido através de tais pesquisas. Este artigo foca o sentido mais estrito da palavra.");
 
-/* INSERINDO DADOS NA TABELA PRODUTO */
+/* INSERINDO DADOS NA TABELA PRODUTO (25 INSERÇÕES)*/
 
 INSERT INTO produto
 VALUES (NULL, "O diário de Anne Frank (edição oficial)", "Livro Biográfico O diário de Anne Frank (edição oficial) - Anne Frank lorem impsu", 26.2, 6, "https://images-na.ssl-images-amazon.com/images/I/91RMqWB-CTL._AC_UL604_SR604,400_.jpg", 3);
@@ -55,14 +55,36 @@ VALUES (NULL, "O Contador de Histórias: Memórias...", "Livro Biográfico O Con
 INSERT INTO produto
 VALUES (NULL, "A PAIXÃO SEGUNDO G. H.", "Livro Biográfico A PAIXÃO SEGUNDO G. H. (EDIÇÃO COMEMORATIVA) - Clarice Lispector lorem impsu", 21, 20, "https://images-na.ssl-images-amazon.com/images/I/71rmK9DYSIL._AC_UL604_SR604,400_.jpg", 3);
 INSERT INTO produto
-VALUES (NULL, "O homem mais rico da Babilônia", "O homem mais rico da Babilônia", 13, 0, "https://images-na.ssl-images-amazon.com/images/I/81HKcvLn15L._AC_UL604_SR604,400_.jpg", 2);
+VALUES (NULL, "O homem mais rico da Babilônia", "Livro Autoajuda O homem mais rico da Babilônia - George S Clason lorem impsu", 13, "0", "https://images-na.ssl-images-amazon.com/images/I/81HKcvLn15L._AC_UL604_SR604,400_.jpg", 2);
 INSERT INTO produto
-VALUES (NULL, "Mente Blindada: 12 Simples...", "Mente Blindada: 12 Simples...", 1.99, 8, "https://images-na.ssl-images-amazon.com/images/I/8…sticker-v7,TopR4,400_.jpg", 2);
+VALUES (NULL, "Mente Blindada: 12 Simples...", "Livro Autoajuda Mente Blindada: 12 Simples Estrat… Cheia De Resultados - Susan Anderson lorem impsu", 1.99, "8", "https://images-na.ssl-images-amazon.com/images/I/8…sticker-v7,TopRight,0,-50_AC_UL604_SR604,400_.jpg", 2);
 INSERT INTO produto
-VALUES (NULL, "Mais esperto que o Diabo", "Mais esperto que o Diabo", 24.89, 10, "https://images-na.ssl-images-amazon.com/images/I/71c4clNHGYL._AC_UL604_SR604,400_.jpg", 2);
+VALUES (NULL, "Mais esperto que o Diabo", "Livro Autoajuda Mais esperto que o Diabo: O misté…iberdade e do sucesso - Napoleon Hill lorem impsu", 24.89, "10", "https://images-na.ssl-images-amazon.com/images/I/71c4clNHGYL._AC_UL604_SR604,400_.jpg", 2);
 INSERT INTO produto
-VALUES (NULL, "Talvez você deva conversar com alguém", "Talvez você deva conversar com alguém", 28.06, 32, "https://images-na.ssl-images-amazon.com/images/IR604,400_.jpg", 2);
+VALUES (NULL, "Talvez você deva conversar com alguém", "Livro Autoajuda Talvez você deva conversar com al…e a vida de todos nós - Lori Gottlieb lorem impsu", 28.06, "32", "https://images-na.ssl-images-amazon.com/images/I/81E-X4rpSdL._AC_UL604_SR604,400_.jpg", 2);
 INSERT INTO produto
-VALUES (NULL, "12 regras para a vida: Um...", "12 regras para a vida: Um...", 34.4, 41, "https://images-na.ssl-images-amazon.com/images/I/51DSFiydE-L._AC_ULages/I/51DSFiydE-L._AC_UL604_SR604,400_.jpg", 2);
+VALUES (NULL, "12 regras para a vida: Um...", "Livro Autoajuda 12 regras para a vida: Um antídoto para o caos - Jordan B. Peterson lorem impsu", 34.4, "41", "https://images-na.ssl-images-amazon.com/images/I/51DSFiydE-L._AC_UL604_SR604,400_.jpg", 2);
 INSERT INTO produto
-VALUES (NULL, "Mindset: A nova psicologia do sucesso", "Mindset: A nova psicologia do sucesso", 27.9, 2, "https://images-na.ssl-images-amazon.com/images/I/71Ils+Co9fL._AC_UL604_SR604,400_.jpg", 2);
+VALUES (NULL, "Mindset: A nova psicologia do sucesso", "Livro Autoajuda Mindset: A nova psicologia do sucesso - Carol S. Dweck lorem impsu", 27.9, "2", "https://images-na.ssl-images-amazon.com/images/I/71Ils+Co9fL._AC_UL604_SR604,400_.jpg", 2);
+INSERT INTO produto
+VALUES (NULL, "Os sete maridos de Evelyn Hugo", "Livro Ficção Os sete maridos de Evelyn Hugo - Taylor Jenkins Reid lorem impsu", 23.9, 10, "https://images-na.ssl-images-amazon.com/images/I/91yEPgRcELL._AC_UL604_SR604,400_.jpg", 1);
+INSERT INTO produto
+VALUES (NULL, "A revolução dos bichos: Um conto de fadas", "Livro Ficção A revolução dos bichos: Um conto de fadas - George Orwell lorem impsu", 12.9, 18, "https://images-na.ssl-images-amazon.com/images/I/91BsZhxCRjL._AC_UL604_SR604,400_.jpg", 1);
+INSERT INTO produto
+VALUES (NULL, "Perigoso!", "Livro Ficção Perigoso! - Tim Warnes lorem impsu", 18.9, 1, "https://images-na.ssl-images-amazon.com/images/I/91W6uZQWTjL._AC_UL604_SR604,400_.jpg", 1);
+INSERT INTO produto
+VALUES (NULL, "Sapiens (Nova edição)", "Livro História Sapiens (Nova edição): Uma breve hi…ria da humanidade - Yuval Noah Harari lorem impsu", 40.9, 1, "https://images-na.ssl-images-amazon.com/images/I/71-ghLb8qML._AC_UL604_SR604,400_.jpg", 4);
+INSERT INTO produto
+VALUES (NULL, "Escravidão - Volume 2", "Livro História Escravidão - Volume 2: Da corrida d…dom João ao Brasil - Laurentino Gomes lorem impsu", 28.2, 2, "https://images-na.ssl-images-amazon.com/images/I/71b8vcM3eiL._AC_UL604_SR604,400_.jpg", 4);
+INSERT INTO produto
+VALUES (NULL, "Grimório das Bruxas (Moon Edition)", "Livro História Grimório das Bruxas (Moon Edition) - Ronald Hutton lorem impsu", 49.9, 4, "https://images-na.ssl-images-amazon.com/images/I/91sXeImEBnL._AC_UL604_SR604,400_.jpg", 4);
+INSERT INTO produto
+VALUES (NULL, "Brasil, uma história", "undefined", 33.34, 12, "https://images-na.ssl-images-amazon.com/images/I/8…sticker-v7,TopRight,0,-50_AC_UL604_SR604,400_.jpg", 4);
+INSERT INTO produto
+VALUES (NULL, "Inteligência emocional", "Livro Ciências Inteligência emocional - Daniel Goleman lorem impsu", 39.9, 6, "https://images-na.ssl-images-amazon.com/images/I/71f9R8hY23L._AC_UL604_SR604,400_.jpg", 5);
+INSERT INTO produto
+VALUES (NULL, "Storytelling com dados", "Livro Ciências Storytelling com dados: Um guia s…de negócios - Cole Nussbaumer Knaflic lorem impsu", 46, 8, "https://images-na.ssl-images-amazon.com/images/I/71sQk9TbNxL._AC_UL604_SR604,400_.jpg", 5);
+INSERT INTO produto
+VALUES (NULL, "Ideias para adiar o fim do mundo", "Livro Ciências Ideias para adiar o fim do mundo (Nova edição) - Ailton Krenak lorem impsu", 16.9, 30, "https://images-na.ssl-images-amazon.com/images/I/71BnFkpryFL._AC_UL604_SR604,400_.jpg", 5);
+
+/* INSERINDO DADOS NA TABELA ENDERECO (10 INSERÇÕES) */
